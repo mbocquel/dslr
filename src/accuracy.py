764869688e88cfa4724af55d33f79698a40ee795
Maxence BOCQUEL <maxence.bocquel@gmail.com>
2024-01-12 15:14:16 +0100

    mandatory finished

--- src/accuracy.py
+++ src/accuracy.py
@@ -1,13 +1,17 @@
-import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score
+from sys import argv
+
 
 def main():
-	df_true = pd.read_csv("dataset_truth.csv", index_col = "Index")
-	df_esti = pd.read_csv("houses.csv", index_col = "Index")
-	print(df_esti.loc[:, "Hogwarts House":])
-	print(df_true.loc[:, "Hogwarts House":])
-	print("accuracy score",accuracy_score(df_true,df_esti))
+    try:
+        assert len(argv) == 3, "You need to pass the path of the Truth file and of the prediction file"
+        df_true = pd.read_csv(argv[1], index_col = "Index")
+        df_esti = pd.read_csv(argv[2], index_col = "Index")
+        print(f'Accuracy score {accuracy_score(df_true,df_esti) * 100}%' )
+    except Exception as e:
+        print(f'Error: {e}')
+
 
 if __name__ == "__main__":
 	main()
