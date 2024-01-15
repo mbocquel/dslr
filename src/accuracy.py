47a66dd3a468fae2a12b6ed8af097afae316dcc2
Pierrick Jay <pierrick.jay1@gmail.com>
2024-01-15 17:08:08 +0100

    fixing stochaostic, need to clean, bonus stochaostic finish

--- src/accuracy.py
+++ src/accuracy.py
@@ -5,8 +5,6 @@ from sklearn.metrics import accuracy_score
 def main():
 	df_true = pd.read_csv("dataset_truth.csv", index_col = "Index")
 	df_esti = pd.read_csv("houses.csv", index_col = "Index")
-	print(df_esti.loc[:, "Hogwarts House":])
-	print(df_true.loc[:, "Hogwarts House":])
 	print("accuracy score",accuracy_score(df_true,df_esti))
 
 if __name__ == "__main__":
--- src/logreg_predict.py
+++ src/logreg_predict.py
@@ -13,45 +13,55 @@ def calc_proba(valuesN, w, b):
 
 def normalize_value(df_test, df_result):
 	test_N = df_test.copy()
+
 	for i in range(len(df_test.columns)):
 		mean = df_result.loc["Mean", df_test.columns[i]]
 		std = df_result.loc["Std", df_test.columns[i]]
 		test_N.loc[:, test_N.columns[i]] = (test_N.loc[:, test_N.columns[i]] - mean) / std
 	return (test_N)
 
-def create_prob_arr(df_test, df_result):
-	normalized_val = normalize_value(df_test, df_result)
-	df_test.loc[:, "Slytherin"] = calc_proba(normalized_val, df_result.loc["Slytherin", :"Flying"], df_result.loc["Slytherin", "b"]) * 100
-	df_test.loc[:, "Gryffindor"] = calc_proba(normalized_val, df_result.loc["Gryffindor", :"Flying"], df_result.loc["Gryffindor", "b"]) * 100
-	df_test.loc[:, "Hufflepuff"] = calc_proba(normalized_val, df_result.loc["Hufflepuff", :"Flying"], df_result.loc["Hufflepuff", "b"]) * 100
-	df_test.loc[:, "Ravenclaw"] = calc_proba(normalized_val, df_result.loc["Ravenclaw", :"Flying"], df_result.loc["Ravenclaw", "b"]) * 100
-	return df_test
 
-def create_house_sheet(df_array):
-	print(df_array.iloc[:, -4:])
+def create_prob_arr(df_test, params):
+	result = df_test.copy()
+	normalized_val = normalize_value(result, params)
+	result.loc[:, "Slytherin"] = calc_proba(normalized_val, params.iloc[2, :-1], params.iloc[2, -1]) * 100
+	result.loc[:, "Ravenclaw"] = calc_proba(normalized_val, params.iloc[3, :-1], params.iloc[3, -1]) * 100
+	result.loc[:, "Gryffindor"] = calc_proba(normalized_val, params.iloc[4, :-1], params.iloc[4, -1]) * 100
+	result.loc[:, "Hufflepuff"] = calc_proba(normalized_val, params.iloc[5, :-1], params.iloc[5, -1]) * 100
+	return result
+
+
+def create_house_sheet(prob_array):
+	df_array = prob_array.copy()
 	last_columns = df_array.iloc[:, -4:]
 	nameHouses = last_columns.idxmax(axis=1)
-	print(nameHouses)
 	df_array["Hogwarts House"] = nameHouses
 	df_finals = pd.DataFrame(df_array.iloc[:, -1:])
-	df_array.to_csv("result.csv")
-	df_finals.to_csv("houses.csv")
-	print(df_array)
+	return df_finals
+
 
 def main():
-	# try:
-		assert len(argv) == 3, "You need to pass your data test as argument and the trained value"
-		df_toTest = pd.read_csv(argv[1], index_col = "Index")
-		assert df_toTest is not None, "There is a problem with the dataset..."
-		df_result = pd.read_csv(argv[2], index_col = "Index")
-		assert df_toTest is not None, "There is a problem with the dataset..."
-		X_toTest = df_toTest.drop(["Hogwarts House", "First Name", "Last Name", "Birthday", "Best Hand", "Astronomy"], axis=1, inplace=False)
-		for i in range(len(X_toTest.columns)):
-			X_toTest.loc[X_toTest.iloc[:,i].isna(), X_toTest.columns[i]] = df_result.loc["Mean", X_toTest.columns[i]]
-		prob_array = create_prob_arr(X_toTest, df_result)
-		create_house_sheet(prob_array)
+    try:
+        assert len(argv) == 3, "You need to pass your data test as argument and the trained value"
+        df_toTest = pd.read_csv(argv[1], index_col = "Index")
+        assert df_toTest is not None, "There is a problem with the dataset..."
+        params = pd.read_csv(argv[2], index_col = 0)
+        assert params is not None, "There is a problem with the dataset..."
 
-	# except Exception as e:
-	# 	print(f'caught {type(e)}: e')
+        col_to_del = [col for col in df_toTest.columns if col not in params.columns]
+        X_toTest = df_toTest.drop(col_to_del, axis=1, inplace=False)
+
+        # Replace missing values with the mean of the feature
+        for i in range(len(X_toTest.columns)):
+            X_toTest.loc[X_toTest.iloc[:,i].isna(), X_toTest.columns[i]] = params.loc["Mean", X_toTest.columns[i]]
+
+        prob_array = create_prob_arr(X_toTest, params)
+        df_finals = create_house_sheet(prob_array)
+        df_finals.to_csv("houses.csv")
+
+    except Exception as e:
+        print(f'caught {type(e)}: e')
+
+
 if __name__ == "__main__":
 	main()
